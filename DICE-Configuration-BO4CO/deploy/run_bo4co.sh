979d95f16c853e001912b4c0fb88237d1a008408
cli5@WIN.DOC.IC.AC.UK <cli5@WIN.DOC.IC.AC.UK>
2017-07-25 10:45:23 +0100

    Update site

--- DICE-Configuration-BO4CO/deploy/run_bo4co.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# script for execution of bo4co
-#
-sudo ./run_main.sh /usr/local/MATLAB/MATLAB_Runtime/v85/
--- DICE-Configuration-BO4CO/src/external/libs/+mls/+internal/HttpResponse.m
+++
@@ -1,7 +0,0 @@
-classdef HttpResponse < handle
-    properties
-        Data = [];
-        ContentType = '';
-        StatusCode = 200;
-    end
-end
